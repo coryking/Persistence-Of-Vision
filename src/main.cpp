89006b6c5d7c95b29389b6ebfbc1480338fb120c
Cory R. King <cory@bluetaka.com>
2025-11-23 11:47:35 -0800

    many updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,30 @@
 #include <Arduino.h>
 #include "esp_timer.h"
+#include <NeoPixelBus.h>
 
-#define FASTLED_ALL_PINS_HARDWARE_SPI
-#define FASTLED_ESP32_SPI_BUS FSPI
-
-#define LED_CLOCK 9
-#define LED_DATA 7
 #define NUM_LEDS 30
-#define LED_DATA_RATE_MHZ 20
 
-#include <FastLED.h>
-CRGB leds[NUM_LEDS];
+// ESP32-S3 hardware SPI for SK9822/APA102 (DotStar)
+// Uses GPIO 7 (data) and GPIO 9 (clock) via hardware SPI
+NeoPixelBus<DotStarBgrFeature, DotStarSpiMethod> strip(NUM_LEDS);
 
 void setup()
 {
     Serial.begin(115200);
     delay(5000);
-    FastLED.addLeds<SK9822, LED_DATA, LED_CLOCK, BGR, DATA_RATE_MHZ(LED_DATA_RATE_MHZ)>(&leds[0], NUM_LEDS);
+    strip.Begin();
     Serial.println("Hello there");
 }
 
 void loop()
 {
-    fill_solid(leds, NUM_LEDS, CRGB::White);
+    // Fill all LEDs with white
+    for(int i = 0; i < NUM_LEDS; i++) {
+        strip.SetPixelColor(i, RgbColor(255, 255, 255));
+    }
+
     uint64_t start = esp_timer_get_time();
-    FastLED.show();
+    strip.Show();
     uint64_t end = esp_timer_get_time();
     Serial.printf("Sample: %llu\n", end - start);
     delay(1000);
