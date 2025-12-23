3de1cd6a40139f7cd30419361f037af8e703deac
Cory R. King <cory@bluetaka.com>
2025-12-23 10:35:52 -0800

    add debug stuff for ir and fix button mapping

--- motor_controller/src/remote_input.cpp
+++ motor_controller/src/remote_input.cpp
@@ -17,27 +17,38 @@ Command remoteInputPoll() {
         return Command::None;
     }
 
+    // Debug: Show raw IR data
+    Serial.printf("[IR] Raw: 0x%04X Type: %d\n", (uint16_t)results.value, results.decode_type);
+
     if (results.decode_type == UNKNOWN) {
+        Serial.println("[IR] UNKNOWN type, ignoring");
         irrecv.resume();
         return Command::None;
     }
 
     uint16_t code = rc5StripToggleBit((uint16_t)results.value);
+    Serial.printf("[IR] Stripped: 0x%04X\n", code);
     irrecv.resume();
 
+    Command cmd = Command::None;
     switch (code) {
-        case SAGETV_BTN_1: return Command::Effect1;
-        case SAGETV_BTN_2: return Command::Effect2;
-        case SAGETV_BTN_3: return Command::Effect3;
-        case SAGETV_BTN_4: return Command::Effect4;
-        case SAGETV_BTN_5: return Command::Effect5;
-        case SAGETV_BTN_6: return Command::Effect6;
-        case SAGETV_BTN_7: return Command::Effect7;
-        case SAGETV_BTN_8: return Command::Effect8;
-        case SAGETV_BTN_9: return Command::Effect9;
-        case SAGETV_BTN_0: return Command::Effect10;
-        case SAGETV_BTN_VOL_UP: return Command::BrightnessUp;
-        case SAGETV_BTN_VOL_DOWN: return Command::BrightnessDown;
-        default: return Command::None;
+        case SAGETV_BTN_1: cmd = Command::Effect1; break;
+        case SAGETV_BTN_2: cmd = Command::Effect2; break;
+        case SAGETV_BTN_3: cmd = Command::Effect3; break;
+        case SAGETV_BTN_4: cmd = Command::Effect4; break;
+        case SAGETV_BTN_5: cmd = Command::Effect5; break;
+        case SAGETV_BTN_6: cmd = Command::Effect6; break;
+        case SAGETV_BTN_7: cmd = Command::Effect7; break;
+        case SAGETV_BTN_8: cmd = Command::Effect8; break;
+        case SAGETV_BTN_9: cmd = Command::Effect9; break;
+        case SAGETV_BTN_0: cmd = Command::Effect10; break;
+        case SAGETV_BTN_VOL_UP: cmd = Command::BrightnessUp; break;
+        case SAGETV_BTN_VOL_DOWN: cmd = Command::BrightnessDown; break;
+        default:
+            Serial.printf("[IR] Unknown code 0x%04X\n", code);
+            return Command::None;
     }
+
+    Serial.printf("[IR] Decoded command: %d\n", (int)cmd);
+    return cmd;
 }
--- shared/sagetv_buttons.h
+++ shared/sagetv_buttons.h
@@ -35,75 +35,75 @@ static inline uint16_t rc5StripToggleBit(uint16_t code) {
 // =============================================================================
 
 // Power and Mode buttons
-#define SAGETV_BTN_POWER        0x30C   // Red power button
-#define SAGETV_BTN_TV           0x321
-#define SAGETV_BTN_GUIDE        0x322
-#define SAGETV_BTN_SEARCH       0x323
-#define SAGETV_BTN_HOME         0x320
-#define SAGETV_BTN_MUSIC        0x324
-#define SAGETV_BTN_PHOTOS       0x325
-#define SAGETV_BTN_VIDEOS       0x326
-#define SAGETV_BTN_ONLINE       0x327
+#define SAGETV_BTN_POWER        0x70C   // Red power button
+#define SAGETV_BTN_TV           0x721
+#define SAGETV_BTN_GUIDE        0x722
+#define SAGETV_BTN_SEARCH       0x723
+#define SAGETV_BTN_HOME         0x720
+#define SAGETV_BTN_MUSIC        0x724
+#define SAGETV_BTN_PHOTOS       0x725
+#define SAGETV_BTN_VIDEOS       0x726
+#define SAGETV_BTN_ONLINE       0x727
 
 // Number pad
-#define SAGETV_BTN_1            0x301
-#define SAGETV_BTN_2            0x302
-#define SAGETV_BTN_3            0x303
-#define SAGETV_BTN_4            0x304
-#define SAGETV_BTN_5            0x305
-#define SAGETV_BTN_6            0x306
-#define SAGETV_BTN_7            0x307
-#define SAGETV_BTN_8            0x308
-#define SAGETV_BTN_9            0x309
-#define SAGETV_BTN_0            0x300
-#define SAGETV_BTN_DASH         0x30A   // Hyphen/dash
-#define SAGETV_BTN_ABC_123      0x30B   // Text input toggle
+#define SAGETV_BTN_1            0x701
+#define SAGETV_BTN_2            0x702
+#define SAGETV_BTN_3            0x703
+#define SAGETV_BTN_4            0x704
+#define SAGETV_BTN_5            0x705
+#define SAGETV_BTN_6            0x706
+#define SAGETV_BTN_7            0x707
+#define SAGETV_BTN_8            0x708
+#define SAGETV_BTN_9            0x709
+#define SAGETV_BTN_0            0x700
+#define SAGETV_BTN_DASH         0x70A   // Hyphen/dash
+#define SAGETV_BTN_ABC_123      0x70B   // Text input toggle
 
 // Channel and volume
-#define SAGETV_BTN_PREV_CH      0x329
-#define SAGETV_BTN_AUDIO        0x328
-#define SAGETV_BTN_MUTE         0x30D
-#define SAGETV_BTN_CH_UP        0x312
-#define SAGETV_BTN_CH_DOWN      0x313
-#define SAGETV_BTN_VOL_UP       0x310
-#define SAGETV_BTN_VOL_DOWN     0x311
+#define SAGETV_BTN_PREV_CH      0x729
+#define SAGETV_BTN_AUDIO        0x728
+#define SAGETV_BTN_MUTE         0x70D
+#define SAGETV_BTN_CH_UP        0x712
+#define SAGETV_BTN_CH_DOWN      0x713
+#define SAGETV_BTN_VOL_UP       0x710
+#define SAGETV_BTN_VOL_DOWN     0x711
 
 // Navigation
-#define SAGETV_BTN_OPTIONS      0x32A
-#define SAGETV_BTN_INFO         0x315
-#define SAGETV_BTN_BACK         0x314
-#define SAGETV_BTN_UP           0x318
-#define SAGETV_BTN_DOWN         0x31A
-#define SAGETV_BTN_LEFT         0x31B
-#define SAGETV_BTN_RIGHT        0x319
-#define SAGETV_BTN_ENTER        0x31C
+#define SAGETV_BTN_OPTIONS      0x72A
+#define SAGETV_BTN_INFO         0x715
+#define SAGETV_BTN_BACK         0x714
+#define SAGETV_BTN_UP           0x718
+#define SAGETV_BTN_DOWN         0x71A
+#define SAGETV_BTN_LEFT         0x71B
+#define SAGETV_BTN_RIGHT        0x719
+#define SAGETV_BTN_ENTER        0x71C
 
 // Special function buttons
-#define SAGETV_BTN_FAVORITE     0x317   // "F" symbol
-#define SAGETV_BTN_WATCHED      0x316   // "M" symbol
+#define SAGETV_BTN_FAVORITE     0x717   // "F" symbol
+#define SAGETV_BTN_WATCHED      0x716   // "M" symbol
 
 // Playback control
-#define SAGETV_BTN_PLAY         0x335
-#define SAGETV_BTN_PAUSE        0x330
-#define SAGETV_BTN_STOP         0x336
-#define SAGETV_BTN_RECORD       0x337   // Red dot
+#define SAGETV_BTN_PLAY         0x735
+#define SAGETV_BTN_PAUSE        0x730
+#define SAGETV_BTN_STOP         0x736
+#define SAGETV_BTN_RECORD       0x737   // Red dot
 
 // Seek and skip
-#define SAGETV_BTN_SKIP_BACK    0x31D
-#define SAGETV_BTN_REWIND       0x332
-#define SAGETV_BTN_FAST_FWD     0x334
-#define SAGETV_BTN_SKIP_FWD     0x31E
-#define SAGETV_BTN_PREV_CHAPTER 0x32B   // |<<
-#define SAGETV_BTN_SKIP_BACK2   0x32B   // Same as prev chapter
-#define SAGETV_BTN_SKIP_FWD2    0x32C
-#define SAGETV_BTN_DOT_TITLE    0x31F
-#define SAGETV_BTN_NEXT_CHAPTER 0x31E   // >>| (same as skip forward)
+#define SAGETV_BTN_SKIP_BACK    0x71D
+#define SAGETV_BTN_REWIND       0x732
+#define SAGETV_BTN_FAST_FWD     0x734
+#define SAGETV_BTN_SKIP_FWD     0x71E
+#define SAGETV_BTN_PREV_CHAPTER 0x72B   // |<<
+#define SAGETV_BTN_SKIP_BACK2   0x72B   // Same as prev chapter
+#define SAGETV_BTN_SKIP_FWD2    0x72C
+#define SAGETV_BTN_DOT_TITLE    0x71F
+#define SAGETV_BTN_NEXT_CHAPTER 0x71E   // >>| (same as skip forward)
 
 // Additional functions
-#define SAGETV_BTN_DELETE       0x32D
-#define SAGETV_BTN_ASPECT       0x32E
-#define SAGETV_BTN_VIDEO_OUT    0x32F
-#define SAGETV_BTN_DVD_MENU     0x33A
-#define SAGETV_BTN_DVD_RETURN   0x33B
+#define SAGETV_BTN_DELETE       0x72D
+#define SAGETV_BTN_ASPECT       0x72E
+#define SAGETV_BTN_VIDEO_OUT    0x72F
+#define SAGETV_BTN_DVD_MENU     0x73A
+#define SAGETV_BTN_DVD_RETURN   0x73B
 
 #endif // SAGETV_BUTTONS_H
